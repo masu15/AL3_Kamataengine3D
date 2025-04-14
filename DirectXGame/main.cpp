9ac294788c87e553caa510647edf892a7c74b849
ます <k024g1021@g.neec.ac.jp>
2025-04-14 12:17:41 +0900

    1_1

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -5,11 +5,16 @@ using namespace KamataEngine;
 // Windowsアプリでのエントリーポイント(main関数)
 
 
-int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int)
+{
 	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
     KamataEngine::Initialize(L"GC2D_06_マスヤマ_リョウタ_AL3");
+
 	GameScene* gameScene = new GameScene();
+
 	gameScene->Initialize();
+
 	while (true) {
 		if (KamataEngine::Update())
 		{
