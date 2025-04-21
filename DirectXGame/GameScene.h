822042c41734021e73748c9385612acfed94dc01
ます <k024g1021@g.neec.ac.jp>
2025-04-21 10:09:50 +0900

    コミット

--- DirectXGame/GameScene.h
+++ DirectXGame/GameScene.h
@@ -8,6 +8,15 @@ public:
 	void Update();
 
 	void Draw();
+
+~GameScene();
+
+private:
+uint32_t textureHandle_ = 0;
+	KamataEngine::Model* model_ = nullptr;
+KamataEngine::WorldTransform worldTransform_;
+
+KamataEngine::Camera camera_;
 };
 
 
